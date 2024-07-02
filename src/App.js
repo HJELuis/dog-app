745fe093f2bd8df52de29c4c717297ae9a8de7ec
HJELuis <lenrique.lejh@gmail.com>
2024-07-02 12:39:33 -0600

    adding style to the Header

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Login from "./components/Login";
 import DogsList from "./components/DogsList";
+import GlobalStyle from "./theme/GlobalStyles";
 
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <GlobalStyle />
+      <Header />      
       <Routes>
         <Route path="/" element={<Login />}/>
         <Route path="/dogs" element={<DogsList/>}/>
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 const AppHeader = styled.header`
     background-color: #EF9C66;
-    font-size: 2em;
-    color: #FFFFFF;    
+    font-size: 1.5em;
+    color: #FFEDDD;    
 `
 
 const Headlight = styled.h1`
