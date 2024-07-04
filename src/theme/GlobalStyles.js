a3676b5722ffbfa9c5ed2a5ad87d14d4ab0e45b7
HJELuis <lenrique.lejh@gmail.com>
2024-07-04 13:05:48 -0600

    using props to set up some style

--- src/theme/GlobalStyles.js
+++ src/theme/GlobalStyles.js
@@ -3,7 +3,10 @@ import  { createGlobalStyle } from "styled-components";
 const GlobalStyle = createGlobalStyle`
     body {
         margin: 0;              
-    }    
+    } 
+    header {
+        background-color: ${props => props.theme.colors.first};
+    }   
 `
 
 export default GlobalStyle;
---
+++ src/theme/index.js
@@ -0,0 +1,10 @@
+const Theme = {
+    colors: {
+        first: "#EF9C66",
+        second: "#FCDC94",
+        third: "C8CFA0",
+        fourth: "78ABA8",
+    }    
+}
+
+export default Theme;
